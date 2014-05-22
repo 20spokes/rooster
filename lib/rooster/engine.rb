e434b75fa35d3c30955df8e1578ab21864fe6fc3
Ryan Fischer <ryan.fischer@20spokes.com>
2014-05-22 15:05:23 -0500

    updated assets

--- lib/rooster/engine.rb
+++ lib/rooster/engine.rb
@@ -2,8 +2,17 @@ module Rooster
   class Engine < ::Rails::Engine
     isolate_namespace Rooster
 
-    initializer :assets do |config|
-      Rails.application.config.assets.precompile += %w( rooster/application.js rooster/application.css )
+#    initializer :assets do |config|
+#      Rails.application.config.assets.precompile += %w( rooster/application.js rooster/application.css )
+#    end
+
+    initializer "RoosterEngine asset precompile", group: :all do |app|
+      app.config.assets.precompile += %w(
+        rooster/rooster.js
+        rooster/rooster.css
+        rooster.js
+        rooster.css
+      )
     end
 
 

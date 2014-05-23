a533610afb84579f0a59211595e77e3de6709dea
Ryan Fischer <ryan.fischer@20spokes.com>
2014-05-22 21:13:59 -0500

    update tinymce asets

--- app/assets/javascripts/rooster/application.js
+++ app/assets/javascripts/rooster/application.js
@@ -1,5 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-//= require rooster/vendor/tinymce/tinymce.min.js
-//= require rooster/vendor/tinymce/jquery.tinymce.min.js
-//= require_tree ./vendor/tinymce

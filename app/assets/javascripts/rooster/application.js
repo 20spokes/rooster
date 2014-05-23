3d536e97b8d79d996ffddfae28b18e2bdb2a08d0
Ryan Fischer <ryan.fischer@20spokes.com>
2014-05-23 11:56:04 -0500

    added tinymce to app

--- app/assets/javascripts/rooster/application.js
+++ app/assets/javascripts/rooster/application.js
@@ -1,4 +1,4 @@
 //= require jquery
 //= require jquery_ujs
-//= require rooster/tinymce/tinymce.min.js
-//= require rooster/tinymce/jquery.tinymce.min.js
+//= require tinymce/tinymce.min.js
+//= require tinymce/jquery.tinymce.min.js

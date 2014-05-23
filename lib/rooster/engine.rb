4d26d9b67c3e7d3c4d3004a735cbce862f76a65e
Ryan Fischer <ryan.fischer@20spokes.com>
2014-05-23 10:24:10 -0500

    test for prod

--- lib/rooster/engine.rb
+++ lib/rooster/engine.rb
@@ -32,7 +32,7 @@ module Rooster
     initializer "static assets" do |app|
       #app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
       #app.middleware.insert_before(::Rack::Lock, ::ActionDispatch::Static, "#{root}/public")
-      app.middleware.use ::ActionDispatch::Static "#{root}/public"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
   end
 end

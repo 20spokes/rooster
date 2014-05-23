ef25d04dd710dcf97743d03a9c07f18a04e2839f
Ryan Fischer <ryan.fischer@20spokes.com>
2014-05-22 19:13:08 -0500

    udpated

--- lib/rooster/engine.rb
+++ lib/rooster/engine.rb
@@ -2,18 +2,18 @@ module Rooster
   class Engine < ::Rails::Engine
     isolate_namespace Rooster
 
-#    initializer :assets do |config|
-#      Rails.application.config.assets.precompile += %w( rooster/application.js rooster/application.css )
-#    end
-
-    initializer "RoosterEngine asset precompile", group: :all do |app|
-      app.config.assets.precompile += %w(
-        rooster/rooster.js
-        rooster/rooster.css
-        rooster.js
-        rooster.css
-      )
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( rooster/application.js rooster/application.css )
     end
+
+#    initializer "RoosterEngine asset precompile", group: :all do |app|
+#      app.config.assets.precompile += %w(
+#        rooster/rooster.js
+#        rooster/rooster.css
+#        rooster.js
+#        rooster.css
+#      )
+#    end
 
 
     config.generators do |generate|
--- lib/rooster/version.rb
+++ lib/rooster/version.rb
@@ -1,3 +1,3 @@
 module Rooster
-  VERSION = "0.0.4.3"
+  VERSION = "0.0.4.4"
 end
